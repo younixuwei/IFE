7c66a86a87d21ae508ead3beba5d5ae1a1c0c46a
文港 <374629354@qq.com>
2018-05-28 10:43:14 +0800

    添加babel配置和loader配置

--- 1.2/src/main.js
+++ 1.2/src/main.js
@@ -3,7 +3,7 @@ import {
     router
 } from 'san-router';
 import San from './app.san';
-
+require('./base.css')
 console.log('我陈一发最牛逼!');
 
 router.add({
--- 1.2/webpack.config.js
+++ 1.2/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/main.js',
     output: {
@@ -9,17 +10,40 @@ module.exports = {
     devServer: {
         contentBase: './dist'
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html' 
-        })
-    ],
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.san$/,
                 use: 'san-loader'
+            }, {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ["es2015", "stage-2"]
+                }
+            },
+            {
+                test: /.css$/,
+                loaders: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
-    }
+    },
+    resolve: {
+        extensions: [".js", ".json", ".san"],
+        alias: {
+            san: process.env.NODE_ENV === 'production' ?
+                'san/dist/san.js' : 'san/dist/san.dev.js'
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
 };
